0b285b4a8362bfa7fe0850b203170317f1947ad9
Tun Lin Thu <thusama@Tuns-MacBook-Pro.local>
2018-04-30 12:49:13 +0900

    Add Book List Viewer and Change folder structure

---
+++ 02. ReduxSimpleStarter - Book List Viewer/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+	return [
+		{ title: "Javascript the good parts", pages: 101 },
+		{ title: "Harry Potter", pages: 20 },
+		{ title: "LOTR", pages: 30 },
+		{ title: "Ruby", pages: 40 },
+		{ title: "The White Tower", pages: 50 }
+	];
+}
